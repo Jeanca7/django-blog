dc7c082a9f3d90b989a8573a8a8bf8eb447c2f71
Jean <jsepulvedadominguez@gmail.com>
2018-11-14 14:47:08 +0000

    added whitenose

--- django_blog/settings/dev.py
+++ django_blog/settings/dev.py
@@ -5,7 +5,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = 'staticfiles'
 
 MEDIA_URL = '/media/'    #for the pictures/images
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  #for resetting my password
 SYSTEM_EMAIL = 'admin@mysitename.com'
